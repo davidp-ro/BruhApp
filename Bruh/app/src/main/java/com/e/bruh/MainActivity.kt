16663e5a2dfc9a01e24dbbcc5252aa3ea249e347
David Pescariu <davidpescariu12@gmail.com>
2020-03-14 16:51:52 +0200

    Added an easter-egg

--- Bruh/app/src/main/java/com/e/bruh/MainActivity.kt
+++ Bruh/app/src/main/java/com/e/bruh/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.e.bruh
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.time.Duration
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        *   TODO: Clicks-per-second
+        *   TODO: Easter-egg-uri
+        */
 
         var bruh = 0 //Bruh...
 
@@ -36,6 +45,19 @@ class MainActivity : AppCompatActivity() {
 
                     val toastAt69 = Toast.makeText(applicationContext, text, duration)
                     toastAt69.show()
+                }
+
+                if(getString(R.string.bruh_counter, bruh) == "420") {
+                    /* Never gonna give you up */
+                    val text = R.string.at420
+                    val duration = Toast.LENGTH_LONG
+
+                    val toastAt420 = Toast.makeText(applicationContext, text, duration)
+                    toastAt420.show()
+
+                    val viewIntent = Intent("android.intent.action.VIEW",
+                                        Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+                                        startActivity(viewIntent)
                 }
             }
         })
